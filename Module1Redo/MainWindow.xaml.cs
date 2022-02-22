24ee669cebdf94d7c0c9f2fb88eac6392441dfe5
AIMAN RAHIM <maiman.ar@live.iium.edu.my>
2022-02-22 11:23:43 +0800

    Add project files.

---
+++ Module1Redo/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Module1Redo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+        private List<Model.Item> itemList;
+        ObservableCollection<Model.OrderDetail> itemList2;
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            joust.Text = counter.ToString();
+            itemList = new List<Model.Item>();
+            itemList2 = new ObservableCollection<Model.OrderDetail>();
+            for(int i = 0; i < 5; i++)
+            {
+                itemList.Add(new Model.Item());
+            }
+            ItemList2.ItemsSource = itemList;
+            ItemList3.ItemsSource = itemList2;
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter--;
+            joust.Text = counter.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            counter++; 
+            joust.Text = counter.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+
+        {
+            
+            if (ItemList3.Items.IsEmpty)
+            {
+                Console.WriteLine("test");
+                if (Int32.Parse(joust.Text) != 0)
+                {
+                    var item = ItemList2.SelectedItem as Model.Item;
+                    itemList2.Add(new Model.OrderDetail(item, Int32.Parse(joust.Text),item.Id));
+                }
+                
+            }
+            else if(!ItemList3.Items.IsEmpty)
+            {
+                Trace.WriteLine("text");
+                var item1 = ItemList2.SelectedItem as Model.Item;
+                var index = from item in itemList2 where item.id.Contains(item1.Id) select itemList2.IndexOf(item);
+                
+                if (index.Any())
+                {
+                    var result = index.First();
+                    Trace.WriteLine(result);
+                    if (result != -1)
+                    {
+                        itemList2[result].Quantity += int.Parse(joust.Text);
+                       
+                    }
+                    
+                }
+                else
+                {
+                    itemList2.Add(new Model.OrderDetail(item1, Int32.Parse(joust.Text), item1.Id));
+                }
+                
+
+            }
+           
+        }
+        private void OnSelected(object sender, RoutedEventArgs e)
+        {
+            ListBox lol = sender as ListBox;
+            var splat = lol.SelectedItem as Model.OrderDetail;
+            quantityBox.Text = splat.Quantity.ToString();
+        }
+
+        private void TextBlock_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Module1Redo/Model/Order.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Module1Redo.Model
+{
+    public class Order
+    {
+        private List<OrderDetail> orderDetailList;
+
+        public List<OrderDetail> OrderDetailList
+        {
+            get { return orderDetailList; }
+            set { orderDetailList = value; }
+        }
+
+
+    }
+    public class Item
+    {
+        public Item(string id,string name,string description,int price)
+        {
+
+        }
+        public Item()
+        {
+            Random _random = new Random();
+            _id = _random.Next(0,10000).ToString();
+             
+        }
+        public override string ToString()
+        {
+            return Id;
+        }
+        private string _id ="default";
+        private string _name="default";
+        private string _description;
+        private string _price;
+        public string Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public string Description { get { return _description; } }
+    }
+    public class OrderDetail:INotifyPropertyChanged
+    {
+        private int _quantity;
+
+        private Item _item;
+        private string _id;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged(object sender, string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public OrderDetail(object selectedItem)
+        {
+            SelectedItem = selectedItem;
+        }
+
+        public OrderDetail(Item selectedItem, int quantity, string id)
+        {
+            _item = selectedItem;
+            Quantity = quantity;
+            _id = selectedItem.Id;
+        }
+        public override string ToString()
+        {
+            onPropertyChanged(this, "OrderDetail");
+            return Quantity.ToString();
+        }
+        public int Quantity { get { return (int)_quantity;} set {
+                _quantity = value;
+                onPropertyChanged(this, "OrderDetail");
+            } }
+        public Item Item { get { return _item; } set { _item = value; } }
+        public string id { get { return _id; } }
+        public object SelectedItem { get; }
+    }
+}
